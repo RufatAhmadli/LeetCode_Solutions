2ee20fca7d457382b1bdd42f96981b8aeb6252e1
Rufat <ahmadlirufat10@gmail.com>
2025-02-03 00:56:55 +0400

    Some problems solved

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        Solution solution=new Solution();
+        Solution solution = new Solution();
         System.out.println(solution.fib(6));
+        String str="Hello World";
+        String str2=str;
+        System.out.println(str);
+        System.out.println(str2);
+        int a=12;
+        int b=7;
+        float c=10f;
+        float d=15f;
+        double e=c/d;
+        System.out.println(a++);
+        System.out.println(++b);
+        System.out.println(a+b);
+        System.out.println(e);
     }
+
     static class Solution {
         public int fib(int n) {
-            if(n==0)return 0;
-            int a=0;
-            int b=1;
-            for(int i=2;i<=n;i++){
-                int copy=b;
-                b=b+a;
-                a=copy;
+            if (n == 0) return 0;
+            int a = 0;
+            int b = 1;
+            for (int i = 2; i <= n; i++) {
+                int copy = b;
+                b = b + a;
+                a = copy;
             }
             return b;
         }

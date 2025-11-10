e1ff8173cf40d4c2331dabe71c6d2a6397ecf4c7
Rufat <ahmadlirufat10@gmail.com>
2025-11-10 16:21:48 +0400

    Solved Question237

---
+++ src/Medium/Question237.java
@@ -0,0 +1,17 @@
+package Medium;
+
+public class Question237 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}

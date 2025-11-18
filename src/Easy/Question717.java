14bb28a5f83b5afd236ac94fc2834732d3dce2fe
Rufat <ahmadlirufat10@gmail.com>
2025-11-18 23:51:07 +0400

    Solved Question717

---
+++ src/Easy/Question717.java
@@ -0,0 +1,15 @@
+package Easy;
+
+public class Question717 {
+    public boolean isOneBitCharacter(int[] bits) {
+        int n=bits.length;
+        int i=0;
+        while(i<n){
+            if(i==n-1) return true;
+            if(bits[i]==1) i+=2;
+            else i++;
+
+        }
+        return false;
+    }
+}
